50cda5a653191d4ac8aab76363a9cb9459e81c04
Asaf Maman <mamanasaf1@gmail.com>
2017-07-30 22:44:41 +0300

    Clean index.js file

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
-
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'react-router-redux'
 import store, { history } from './store'
 import App from './containers/app'
-
 import './index.css'
+import registerServiceWorker from './registerServiceWorker';
 
-const target = document.querySelector('#root')
+//const target = document.querySelector('#root')
 
 render(
   <Provider store={store}>
@@ -26,5 +17,6 @@ render(
       </div>
     </ConnectedRouter>
   </Provider>,
-  target
-)
+  document.getElementById('root')
+);
+registerServiceWorker();
